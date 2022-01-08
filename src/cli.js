4859969fb4cfd78c3f8933d281ce7afa3e2611c5
olekhshch <oleh.shchevrovskyi@gmail.com>
2022-01-08 21:43:53 +0100

    git actions again

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
-import readline from'readline-sync';
+import readline from 'readline-sync';
 
-export const greeting = () => {
-    let userName = readline.question('May I have your name? ');
-    console.log('Hello, ' + userName + '!');
-}
+export default () => {
+  const userName = readline.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+};
